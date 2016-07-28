80e26d2389b9f4826f43174fb40ced4e52304601
Samantha DelBrocco <delbrocco.sam@gmail.com>
2016-07-28 18:16:23 -0400

    logic testing program

---
+++ StringCalculatorKata/StringCalculatorKata.txt/CalculatorTests.cs
@@ -0,0 +1,79 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorKata.txt
+{
+    [TestFixture]
+    class CalculatorTests
+    {
+        [Test]
+        public void Step1_NoParameters()
+        {
+            Calculator calc = new Calculator();
+            int result = calc.Add("");
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestCase("",0)]
+        [TestCase("1", 1)]
+        [TestCase("17", 17)]
+        public void Step1_OneParameter(string numbers, int expected)
+        {
+            Calculator calc = new Calculator();
+            int result = calc.Add(numbers);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("1,2", 3)]
+        [TestCase("1234, 10000", 11234)]
+        public void Step1_TwoParameters(string numbers, int expected)
+        {
+            Calculator calc = new Calculator();
+            int result = calc.Add(numbers);
+
+            Assert.AreEqual(expected, result);
+        }
+        [TestCase("1, 2, 3, 4", 10)]
+        [TestCase("1\n2, 3", 6)]
+        public void Step2(string numbers, int expected)
+        {
+            Calculator calc = new Calculator();
+            int result = calc.Add(numbers);
+
+            Assert.AreEqual(expected, result);
+        }
+        [TestCase("//;\n1;2",3)]
+        [TestCase("//|\n1|2", 3)]
+        public void Step4(string numbers, int expected)
+        {
+            Calculator calc = new Calculator();
+            int result = calc.Add(numbers);
+
+            Assert.AreEqual(expected, result);
+        }
+        [TestCase("-1,2", "Negatives not allowed: -1")]
+        [TestCase("2,-4,3,-5", "Negatives not allowed: -4,-5")]
+        public void step5(string numbers, string expected)
+        {
+            Calculator calc = new Calculator();
+            try
+            {
+                int result = calc.Add(numbers);
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(expected, e.Message);
+                return;
+            }
+
+            Assert.Fail();
+        }
+}
+}
---
+++ StringCalculatorKata/StringCalculatorKata/Calculator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorKata
+{
+    public class Calculator
+    {
+        public int Add (string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+            else
+            {
+                char[] delimiters;
+                var errorMessage = "Negatives not allowed: ";
+                if (!numbers.StartsWith("//"))
+                {
+                    delimiters = new char[] { ',', '\n'};
+                }
+                else
+                {
+                    string[] parts = numbers.Split('\n');
+                    numbers = parts[1];
+                    delimiters = parts[0].Substring(2).ToCharArray();
+                }
+                string[] individualNumbers = numbers.Split(delimiters);
+
+                int result = 0;
+
+                for (int i = 0; i < individualNumbers.Length; i++)
+                {
+                    var x = int.Parse(individualNumbers[i]);
+                    if(x > 0)
+                    {
+                        result += x;
+                    }
+                    else if (x > 1000)
+                    {
+                    
+                    }
+                    else
+                    {
+                        errorMessage += x.ToString();
+                        throw new Exception(errorMessage);
+                    }
+                    
+                }
+
+            return result;
+            }
+        }
+    }
+}
